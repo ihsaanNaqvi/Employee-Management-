d2201f6cc2d59d13ac1d217f6064238a7f97df56
ihsaan.abbas <ihsaan.abbas@SID-IHSAAN-ABBA>
2023-04-28 16:36:16 +0500

    Add project files.

---
+++ practiceapp/IloggerService.cs
@@ -0,0 +1,21 @@
+﻿namespace practiceapp
+{
+    using Microsoft.Extensions.Logging;
+
+    public class MyService
+    {
+        private readonly ILogger<MyService> _logger;
+
+        public MyService(ILogger<MyService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoSomething()
+        {
+            // Log an informational event
+            _logger.LogInformation("Something happened!");
+        }
+    }
+
+}
